54b4e0ca5dcc9dd275c917744606157cd8031494
vishweshwarmaroor05 <vishumaroor@gmail.com>
2026-05-26 11:08:10 +0530

    heap que

---
+++ Heaps/smallest_range_in_k_heaps.cpp
@@ -0,0 +1,33 @@
+/*
+You have k lists of sorted integers in non-decreasing order. Find the smallest range that includes at least one number from each of the k lists.
+
+We define the range [a, b] is smaller than range [c, d] if b - a < d - c or a < c if b - a == d - c.
+*/
+
+#include<iostream>
+#include<vector>
+#include<algorithm>
+#include<vector>
+#include<queue>
+using namespace std;
+class Solution {
+public:
+    vector<int>  checker(vector<vector<int>*> &list,vector<int>& ans){
+        
+
+    }
+    vector<int> smallestRange(vector<vector<int>>& nums) {
+        int numsize=nums.size();
+        vector<vector<int>*> numspointer;
+        for(int i=0;;i<nums.size();i++){
+            numspointer.push_back(&nums[i]);
+        }
+        vector<int> ans(3);
+        auto mini=min_element(numspointer.begin(),numspointer.end());
+        auto maxi=max_element(numspointer.begin(),numspointer.end());
+        ans[0]=*mini;
+        ans[1]=*maxi;
+        ans[3]=ans[1]-ans[2];
+
+    }
+};
